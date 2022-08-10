413497e2123133c398ab56d206305c247a0960f0
Sergio Berdeja <sergioberdeja@gmail.com>
2022-08-10 12:48:53 -0600

    deleted semantic imports

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,17 @@
 import React from 'react'
-import {Menu} from 'semantic-ui-react'
-import {NavLink} from 'react-router-dom'
-import 'semantic-ui-css/semantic.min.css'
+import { NavLink } from 'react-router-dom'
 
 function NavBar() {
-  return (
-    <Menu>
-        <Menu.Item>
+    return (
+        <div>
             <NavLink to='/'>
                 Main
             </NavLink>
-        </Menu.Item>
-        <Menu.Item>
             <NavLink to='/edit'>
                 Edit Page
             </NavLink>
-        </Menu.Item>
-    </Menu>
-  )
+        </div>
+    )
 }
 
 export default NavBar
