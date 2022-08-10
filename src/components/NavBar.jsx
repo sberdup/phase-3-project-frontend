e289a3f04e94a3b72fd36bf09c6ff86af69fde0b
Sergio Berdeja <sergioberdeja@gmail.com>
2022-08-10 13:36:14 -0600

    incorporated semantic patch, fixed Routes issue

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
-// import {Menu} from 'semantic-ui-react'
-// import {NavLink} from 'react-router-dom'
+import {Menu} from 'semantic-ui-react'
+import {NavLink} from 'react-router-dom'
 
 function NavBar() {
   return (
-    <h1>Hi</h1>
-    // <Menu>
-    //     <Menu.Item>
-    //         <NavLink to='/'>
-    //             Main
-    //         </NavLink>
-    //     </Menu.Item>
-    //     <Menu.Item>
-    //         <NavLink to='/edit'>
-    //             Edit Page
-    //         </NavLink>
-    //     </Menu.Item>
-    // </Menu>
+    <Menu>
+        <Menu.Item>
+            <NavLink to='/'>
+                Main
+            </NavLink>
+        </Menu.Item>
+        <Menu.Item>
+            <NavLink to='/edit'>
+                Edit Page
+            </NavLink>
+        </Menu.Item>
+    </Menu>
   )
 }
 
