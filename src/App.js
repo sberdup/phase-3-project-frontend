5b585d412f229f4ad551f154745ea9553efca668
Sergio Berdeja <sergioberdeja@gmail.com>
2022-08-10 12:44:51 -0600

    tried patching, unsuccessful

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 // import logo from './logo.svg';
-// import './App.css';
-import 'semantic-ui-css/semantic.min.css'
+import './App.css';
 import {Routes, Route} from 'react-router-dom'
 import EditPage from './routes/EditPage'
 import LandingPage from './routes/LandingPage'
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import {Menu} from 'semantic-ui-react'
-import {NavLink} from 'react-router-dom'
-import 'semantic-ui-css/semantic.min.css'
+// import {Menu} from 'semantic-ui-react'
+// import {NavLink} from 'react-router-dom'
 
 function NavBar() {
   return (
-    <Menu>
-        <Menu.Item>
-            <NavLink to='/'>
-                Main
-            </NavLink>
-        </Menu.Item>
-        <Menu.Item>
-            <NavLink to='/edit'>
-                Edit Page
-            </NavLink>
-        </Menu.Item>
-    </Menu>
+    <h1>Hi</h1>
+    // <Menu>
+    //     <Menu.Item>
+    //         <NavLink to='/'>
+    //             Main
+    //         </NavLink>
+    //     </Menu.Item>
+    //     <Menu.Item>
+    //         <NavLink to='/edit'>
+    //             Edit Page
+    //         </NavLink>
+    //     </Menu.Item>
+    // </Menu>
   )
 }
 
