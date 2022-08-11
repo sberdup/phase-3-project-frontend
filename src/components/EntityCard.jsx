c9e0ad6918fdfe3d43be210175f81730fac96ad8
Sergio Berdeja <sergioberdeja@gmail.com>
2022-08-11 15:04:21 -0600

    display cards satisfactory

--- src/components/EntityCard.jsx
+++ src/components/EntityCard.jsx
@@ -3,17 +3,19 @@ import { Card, Image } from 'semantic-ui-react'
 import {useState} from 'react'
 
 function EntityCard({entryData}) {
-    // very cool destructuring, shout out to Isaiah
-
+    
     const [cardExpanded, setCardExpanded] = useState(false)
-
+    
+    // very cool destructuring, shout out to Isaiah
     const { id, category_id, description, image, name, logged, cooking_effect, hearts_recovered, attack, defense, drops } = entryData
     
     let category
     switch (category_id) {
-        case 1||2:
+        case 1:
+        case 2:
             category = 'Creature'
             break
+            //can have multiple cases lead to one break, tremendous
         case 3:
             category = 'Monster'
             break
@@ -29,9 +31,19 @@ function EntityCard({entryData}) {
         default:
             category = 'null'
     }
+
     function capitalizeWords(string) {
+        //altered this function to fix Guardian Scout Ii e.g.
+        const newString = string.split(' ').map(str => {
+            if (str === 'ii' || str === 'iii' || str === 'iv') {
+                return str.toUpperCase()
+            } 
+            return str.charAt(0).toUpperCase() + str.slice(1)
+        })
+        return newString.join(' ')
     }
-    const capitalName = name.split(' ').map(str => str.charAt(0).toUpperCase() + str.slice(1)).join(' ')
+    //abstracted capitilization to function instead of below
+    // const capitalName = name.split(' ').map(str => str.charAt(0).toUpperCase() + str.slice(1)).join(' ')
 
     return (
         <Card onClick={() => {
@@ -39,17 +51,17 @@ function EntityCard({entryData}) {
         }}>
             <Card.Content>
                 <Image src={image} />
-                <Card.Header>{capitalName}</Card.Header>
+                <Card.Header>{capitalizeWords(name)}</Card.Header>
 
                 {cardExpanded ? <Card.Meta>#{id} / {category}</Card.Meta> : null}
 
                 <Card.Description>{description}</Card.Description>
 
-                {cardExpanded && cooking_effect ? <Card.Content>Cooking Effect: {cooking_effect}</Card.Content> : null}
+                {cardExpanded && cooking_effect ? <Card.Content>Cooking Effect: {capitalizeWords(cooking_effect)}</Card.Content> : null}
                 {cardExpanded && hearts_recovered ? <Card.Content>Hearts Recovered: {hearts_recovered}</Card.Content> : null}
                 {cardExpanded && attack ? <Card.Content>Attack: {attack}</Card.Content> : null}
                 {cardExpanded && defense ? <Card.Content>Defense: {defense}</Card.Content> : null}
-                {cardExpanded && drops ? <Card.Content>Drops:{drops}</Card.Content> : null}
+                {cardExpanded && drops ? <Card.Content>Drops: {typeof drops === 'string' ? capitalizeWords(drops) : drops.map(drop => capitalizeWords(drop))}</Card.Content> : null}
 
 
                 {cardExpanded ? <Card.Content extra>Logged: {logged ? 'Yes' : 'No'}</Card.Content> : null}
