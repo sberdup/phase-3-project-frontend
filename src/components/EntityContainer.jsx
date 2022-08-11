a29146260f3593ed9dcba979b82b68a2133fd789
Sergio Berdeja <sergioberdeja@gmail.com>
2022-08-11 17:16:34 -0600

    filter field satisfactory

--- src/components/EntityContainer.jsx
+++ src/components/EntityContainer.jsx
@@ -3,16 +3,33 @@ import FilterField from './FilterField'
 import SearchForm from './SearchForm'
 import { Card } from 'semantic-ui-react'
 import EntityCard from './EntityCard'
+import { useState, useEffect } from 'react'
+import { apiURL } from '../App'
 
 function EntityContainer({allEntries, editMode}) {
     console.log(allEntries)
+    const [activeFilter, setActiveFilter] = useState('all')
+    const [subset, setSubset] = useState([])
+
+    useEffect(() => {
+        async function getFetch() {
+            let resp = await fetch(apiURL+`category/${activeFilter}`)
+            resp = await resp.json()
+            setSubset(resp)
+        }
+        if (activeFilter !== 'all') {
+            getFetch()
+        }
+    }, [activeFilter])
+    
+    let currentData = (activeFilter === 'all' ? allEntries : subset) 
+    debugger
     return (
         <div>
-            EntityContainer
-            <FilterField />
+            <FilterField activeFilter={activeFilter} setActiveFilter={setActiveFilter}/>
             <SearchForm />
-            <Card.Group>
-                {allEntries.map((entry) => (
+            <Card.Group centered>
+                {currentData.map((entry) => (
                     <EntityCard key={entry.id} entryData={entry} editMode={editMode}/>
                 ))}
             </Card.Group>
--- src/components/FilterField.jsx
+++ src/components/FilterField.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Menu } from 'semantic-ui-react'
 
-function FilterField() {
-  return (
-    <div>FilterField</div>
-  )
+function FilterField({activeFilter, setActiveFilter}) {
+    // const [categories, setCategories] = useState([])
+
+    const colors = ['grey', 'green', 'blue', 'orange', 'red', 'violet', 'purple', 'yellow',]
+    const categories = ['all', 'all_creatures', "food_creature", "non_food_creature","monster","equipment","material","treasure"]
+
+    function capitalizeCategory(string) {
+        const newString = string.split('_').map(str => {
+            return str.charAt(0).toUpperCase() + str.slice(1)
+        })
+        return newString.join(' ')
+    }
+
+    function clickFilterHandler() {
+        console.log(this)
+        setActiveFilter(this.name)
+    }
+    return (
+        <Menu widths={categories.length}>
+            {categories.map((category, idx) => (
+                <Menu.Item color={colors[idx]} key={category} name={category} onClick={clickFilterHandler} active={category === activeFilter}>
+                    {capitalizeCategory(category)}
+                </Menu.Item>
+            ))}
+        </Menu>
+    )
 }
 
 export default FilterField
