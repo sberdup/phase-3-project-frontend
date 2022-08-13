39208e1d282b68076ca1d97dec5100a373633bcf
Sergio Berdeja <sergioberdeja@gmail.com>
2022-08-13 17:19:34 -0600

    possible issue with useref

--- src/routes/LocationFinder.jsx
+++ src/routes/LocationFinder.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { useState } from 'react'
-import { Grid, Divider, Card } from 'semantic-ui-react'
+import { Grid, Divider, Card, Header, Container } from 'semantic-ui-react'
 import EntityContainer from '../components/EntityContainer'
 import EntityCard from '../components/EntityCard'
+import { useEffect, useRef } from 'react'
+import { apiURL } from '../App'
 
 function LocationFinder({ allEntries }) {
     const [entryList, setEntryList] = useState([])
@@ -12,6 +14,41 @@ function LocationFinder({ allEntries }) {
         debugger
         setEntryList([...entryList].filter(entry => entry !== entryId))
     }
+    
+    const entityLocations = useRef([])
+    useEffect(() => {
+        async function getLocations(id) {
+            let entryData = await fetch(apiURL + `entry/${id}`)
+            entryData = await entryData.json()
+            entityLocations.current = await entryData.locations.map(loc => loc.name)
+            debugger
+        }
+        function uniqueLocations(locationsArray) {
+            debugger
+            let i = locationsArray.length
+            const allLocations = locationsArray.flat()
+            if (i < entryList.length) {
+                return 'No Common Locations'
+            }
+            const commonLocations = []
+            const tally = {}
+            allLocations.forEach(location => {
+                if (tally[location] === i - 1) {
+                    tally[location]++
+                    commonLocations.push(location)
+                } else if (!tally[location]) {
+                    tally[location] = 1
+                } else {
+                    tally[location]++
+                }
+            })
+            return commonLocations
+        }
+        const allLocations = entryList.map(entryId => getLocations(entryId))
+        debugger
+        entityLocations.current = uniqueLocations(allLocations)
+        debugger
+    }, [entryList])
     return (
         <>
             <Divider />
@@ -21,13 +58,18 @@ function LocationFinder({ allEntries }) {
                         <Grid.Row centered>
                             <Card.Group>
                                 {entriesToMap.map((entry) => (
-                                    <EntityCard key={entry.id} entryData={entry} trackClickHandler={trackClickHandler}/>
+                                    <EntityCard key={entry.id} entryData={entry} trackClickHandler={trackClickHandler} />
                                     // Was handing this specific entity card locateMode which caused an error due to two conflicting click events
                                     // Don't hand components props they don't need
                                 ))}
                             </Card.Group>
                         </Grid.Row>
                     </Grid>
+                    <Container>
+                        <Header size='medium'> Locations for the above:</Header>
+                        <p>{entityLocations.current.join(' ')}</p>
+                    </Container>
+
                     <Divider />
                 </>
             }
